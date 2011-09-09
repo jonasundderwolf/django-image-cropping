5cfad927806ca98d63ac84dc39e03295cdb2881d
andreas <andreas.rieger@gmail.com>
2011-09-09 15:05:34 +0200

    adjusts thumbnail processor to recent changes in image_cropping.js

--- image_cropping/thumbnail_processors.py
+++ image_cropping/thumbnail_processors.py
@@ -1,18 +1,17 @@
 def crop_corners(image, box=None, **kwargs):
     """
-    Crop at defined corners, saved as promille values
+    Crop corners to the selection defined by image_cropping
     """
+
     if box:
         values = [int(x) for x in box.split(',')]
-        rx, ry = image.size
-        rx = rx / 1000.0
-        ry = ry / 1000.0
         box = (
-            int(values[0] * rx),
-            int(values[1] * ry),
-            int(values[2] * rx),
-            int(values[3] * ry),
+            int(values[0]),
+            int(values[1]),
+            int(values[2]),
+            int(values[3]),
         )
         image = image.crop(box)
 
     return image
+
