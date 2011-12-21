bddecd0e6a4c8ca21c2dc11fc6f7cdd9ccb6a2d7
Jonas <jvp@jonasundderwolf.de>
2011-12-21 11:12:11 +0100

    More safety checks when cropping

--- image_cropping/thumbnail_processors.py
+++ image_cropping/thumbnail_processors.py
@@ -13,7 +13,7 @@ def crop_corners(image, box=None, **kwargs):
             values = [int(x) for x in box.split(',')]
             width = abs(values[2] - values[0])
             height = abs(values[3] - values[1])
-            if width != image.size[0] or height != image.size[1]:
+            if width and height and (width != image.size[0] or height != image.size[1]):
                 image = image.crop(values)
         except (ValueError, IndexError):
             # There's garbage in the cropping field, ignore
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(name = "django-image-cropping",
-    version = "0.3.5",
+    version = "0.3.6",
     description = "A reusable app for cropping images easily and non-destructively in Django",
     long_description=open('README.rst').read(),
     author = "jonasvp",
