06ac7b3c5cf1687200ca18f86a3886bead995eef
Alexander frenzel <alex@relatedworks.com>
2012-04-05 14:44:09 +0200

    fix CropForeignKey

--- image_cropping/admin.py
+++ image_cropping/admin.py
@@ -7,6 +7,6 @@ class ImageCroppingAdmin(object):
         formfield = super(ImageCroppingAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
         if hasattr(db_field, 'related') and db_field.name in self.model.crop_fk_fields:
-            formfield.widget = CropForeignKeyWidget(db_field.rel, field_name=db_field.name,
+            formfield.widget = CropForeignKeyWidget(db_field.rel, field_name=db_field.field_name,
                                                     admin_site=self.admin_site)
         return formfield
