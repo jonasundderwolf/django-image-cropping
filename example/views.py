c27075e85f4618846335b708071889ec1974b900
Alexander frenzel <alex@relatedworks.com>
2012-04-05 15:40:26 +0200

    catch Exception on empty DB

--- example/views.py
+++ example/views.py
@@ -7,13 +7,13 @@ def form(request):
     try:
         image = Image.objects.all()[0]
         form = ImageForm(instance=image)
-    except Image.DoesNotExist:
+    except (Image.DoesNotExist, IndexError,):
         image = None
         form = ImageForm
 
     try:
         imagefk = ImageFK.objects.all()[0]
-    except ImageFK.DoesNotExist:
+    except (ImageFK.DoesNotExist, IndexError,):
         imagefk = None
 
     return render(request, 'form.html', {'form': form, 'image': image, 'imagefk': imagefk})
