5804936715baf944d5279ecb2dd70acf8596b78f
Alexander frenzel <alex@relatedworks.com>
2012-04-05 12:57:34 +0200

    display cropped image

--- example/views.py
+++ example/views.py
@@ -7,4 +7,4 @@ def form(request):
     image = Image.objects.all()[0]
     form = ImageForm(instance=image)
 
-    return render(request, 'form.html', {'form': form})
+    return render(request, 'form.html', {'form': form, 'image': image})
