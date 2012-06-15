45838e383cec9b7857591bd25ba0e43e44517a10
Alexander frenzel <alex@relatedworks.com>
2012-06-15 09:46:24 +0200

    remove whitespace

--- image_cropping/admin.py
+++ image_cropping/admin.py
@@ -7,7 +7,7 @@ class ImageCroppingAdmin(object):
         formfield = super(ImageCroppingAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
         if hasattr(db_field, 'related') and db_field.name in self.model.crop_fk_fields.keys():
-            formfield.widget = CropForeignKeyWidget(db_field.rel, 
+            formfield.widget = CropForeignKeyWidget(db_field.rel,
                                                     field_name=self.model.crop_fk_fields[db_field.name],
                                                     admin_site=self.admin_site)
         return formfield
