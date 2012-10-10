79521ca57f1e3ba922aa74bbf4439991324b4c70
andreas <andreas.rieger@gmail.com>
2012-10-10 06:41:00 +0200

    Remove superfluous test

--- example/tests.py
+++ example/tests.py
@@ -86,19 +86,14 @@ class BrowserTestCase(LiveServerTestCase):
         self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
         WebDriverWait(self.selenium, 10)
 
-    def test_login(self):
-        """Test if the admin user is able to login."""
-        self.login()
-        body = self.selenium.find_element_by_tag_name('body')
-        self.assertIn('Site administration', body.text)
 
     def test_admin_cropping(self):
-        """Test if the the cropping thumbnail gets correctly embedded in the admin."""
+        """Test if the thumbnail for cropping images gets correctly embedded in the admin."""
         image = create_cropped_image()
         self.login()
         edit_view = reverse('admin:example_image_change', args=[image.pk,])
         self.selenium.get('%s%s' % (self.live_server_url, edit_view))
-        WebDriverWait(self.selenium, 5)
+        WebDriverWait(self.selenium, 10)
         thumbnail = self.selenium.find_element_by_css_selector('.jcrop-holder img')
         thumbnail_source = thumbnail.get_attribute('src')
         self.assertTrue(image.image_field.url in thumbnail_source)
