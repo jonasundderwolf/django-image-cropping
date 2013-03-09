a6bafb8af90a82ac95dfa26df24c114cb3808cb8
andreas <andreas.rieger@gmail.com>
2013-03-09 13:40:17 +0100

    Update required version of easy-thumbnails
    So we avoid deprecation warnings with Django 1.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name = "django-image-cropping",
     packages = find_packages(),
     include_package_data=True,
     install_requires = [
-        'easy_thumbnails==1.1',
+        'easy_thumbnails==1.2',
     ],
     test_suite='example.runtests.runtests',
     classifiers = [
