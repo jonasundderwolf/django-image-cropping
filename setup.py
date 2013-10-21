4ea9506142d3e53618634783e367b403aa6e1107
andreas <andreas.rieger@gmail.com>
2013-10-21 13:12:35 +0200

    Only define version string in one place

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
+import re
+import os
+import codecs
 from distutils.core import setup
 from setuptools import find_packages
 
+
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name="django-image-cropping",
-    version="0.7",
+    version=find_version("image_cropping", "__init__.py"),
     description="A reusable app for cropping images easily and non-destructively in Django",
     long_description=open('README.rst').read(),
     author="jonasvp",
