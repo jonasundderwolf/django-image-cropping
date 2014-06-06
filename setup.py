6806bc7eef85ad6f619d1945e08ca70f47847c3b
Jonas <jvp@jonasundderwolf.de>
2014-06-06 22:30:02 +0200

    Show Python 3 support in classifiers

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
     ],
 )
