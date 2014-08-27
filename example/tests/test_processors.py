92eaa37a4ca21e572814e0e3ea29d939b43ea5ca
andreas <andreas.rieger@gmail.com>
2014-08-27 12:11:36 +0200

    Make assertImagesEqual a bit more verbose

--- example/tests/test_processors.py
+++ example/tests/test_processors.py
@@ -11,10 +11,15 @@ class ProcessorTestCase(TestCase):
         self.image = create_pil_image()
 
     def assertImagesEqual(self, im1, im2, msg=None):
-        if im1.size != im2.size or (
-                ImageChops.difference(im1, im2).getbbox() is not None):
-            raise self.failureException(
-                msg or 'The two images were not identical')
+        if im1.size != im2.size:
+            raise self.failureException(msg or "The two images have different dimensions")
+        if im1.mode != im2.mode:
+            msg = msg or 'The two images have different modes'
+            raise self.failureException('%s: %s vs. %s' % (msg, im1.mode, im2.mode))
+        if ImageChops.difference(im1, im2).getbbox() is not None:
+            msg = msg or 'The two images were not identical'
+            diff = ImageChops.difference(im1, im2).getbbox()
+            raise self.failureException('%s: %s' % (msg, diff))
 
     def test_cropping_with_invalid_box(self):
         """Test if an invalid box is ignored and the image is simply passed through"""
