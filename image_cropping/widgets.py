76c4ce0c691f53ae2d335c0629a2bcafe0a11b23
Piotr Mońko <piotrek.monko@gmail.com>
2014-12-30 19:22:06 +0100

    conditional jquery inclusions, global jcrop api variable

--- image_cropping/widgets.py
+++ image_cropping/widgets.py
@@ -53,13 +53,17 @@ def get_attrs(image, name):
 
 class CropWidget(object):
     class Media:
-        js = (,)
         if settings.IMAGE_CROPPING_JQUERY_URL:
-            js += (settings.IMAGE_CROPPING_JQUERY_URL,)
-        js += (
-            "image_cropping/js/jquery.Jcrop.min.js",
-            "image_cropping/image_cropping.js",
-        )
+            js = (
+                settings.IMAGE_CROPPING_JQUERY_URL,
+                "image_cropping/js/jquery.Jcrop.min.js",
+                "image_cropping/image_cropping.js",
+            )
+        else:
+            js = (
+                "image_cropping/js/jquery.Jcrop.min.js",
+                "image_cropping/image_cropping.js",
+            )
         css = {'all': ("image_cropping/css/jquery.Jcrop.min.css",
                        "image_cropping/css/image_cropping.css",)}
 
