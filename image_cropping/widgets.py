b9011bea99f660b8d64b0fb52bbfc27d7a64e09e
andreas <andreas.rieger@gmail.com>
2015-01-07 18:00:22 +0100

    Simplify defining js scripts in widget media class

--- image_cropping/widgets.py
+++ image_cropping/widgets.py
@@ -55,17 +55,14 @@ def get_attrs(image, name):
 
 class CropWidget(object):
     class Media:
+        js = [
+            "image_cropping/js/jquery.Jcrop.min.js",
+            "image_cropping/image_cropping.js",
+        ]
+
         if settings.IMAGE_CROPPING_JQUERY_URL:
-            js = (
-                settings.IMAGE_CROPPING_JQUERY_URL,
-                "image_cropping/js/jquery.Jcrop.min.js",
-                "image_cropping/image_cropping.js",
-            )
-        else:
-            js = (
-                "image_cropping/js/jquery.Jcrop.min.js",
-                "image_cropping/image_cropping.js",
-            )
+            js.insert(0, settings.IMAGE_CROPPING_JQUERY_URL)
+
         css = {'all': ("image_cropping/css/jquery.Jcrop.min.css",
                        "image_cropping/css/image_cropping.css",)}
 
