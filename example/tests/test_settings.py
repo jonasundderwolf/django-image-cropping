ba964e3778e11a8727949362fafc5e2fba14027f
andreas <andreas.rieger@gmail.com>
2015-03-01 14:18:22 +0100

    Fix settings test case

--- example/tests/test_settings.py
+++ example/tests/test_settings.py
@@ -4,20 +4,20 @@ from django.test.utils import override_settings
 from . import factory
 
 
-class AdminTestCase(WebTest):
+class SettingsTestCase(WebTest):
 
     def setUp(self):
-        super(AdminTestCase, self).setUp()
+        super(SettingsTestCase, self).setUp()
         self.user = factory.create_superuser()
         self.image = factory.create_cropped_image()
 
     def test_jquery_included(self):
         edit_view = reverse('admin:example_image_change', args=[self.image.pk])
         response = self.app.get(edit_view, user=self.user)
-        self.assertTrue('src="/static/js/jquery.min.js">' in response.content)
+        self.assertContains(response, 'src="/static/js/jquery.min.js"')
 
     @override_settings(IMAGE_CROPPING_JQUERY_URL=None)
     def test_jquery_not_included(self):
         edit_view = reverse('admin:example_image_change', args=[self.image.pk])
         response = self.app.get(edit_view, user=self.user)
-        self.assertFalse('src="/static/js/jquery.min.js">' in response.content)
+        self.assertNotContains(response, 'src="/static/js/jquery.min.js"')
