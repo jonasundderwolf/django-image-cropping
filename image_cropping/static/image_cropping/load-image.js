6a700a26a8c3fd360ba04bf2a86fd289f5a72a51
lst <lst@nicestudio.com.tw>
2016-10-14 15:34:50 +0800

    Fix IE11 can not be work

--- image_cropping/static/image_cropping/load-image.js
+++ image_cropping/static/image_cropping/load-image.js
@@ -1,3 +1,16 @@
+if (!String.prototype.endsWith) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+    String.prototype.endsWith = function (searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.lastIndexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+}
+
 (function ($) {
     var className = 'image-ratio';
 
