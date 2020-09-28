68ec5af206155d2b45f3a9031a9d483ae5c24fa3
Matthieu Rigal <matthieu.rigal@jonasundderwolf.de>
2020-09-28 12:56:37 +0200

    Remove hardcoded JQUERY_URL setting from the app config

--- image_cropping/config.py
+++ image_cropping/config.py
@@ -4,7 +4,6 @@ from django.conf import settings
 
 
 class ImageCroppingAppConf(AppConf):
-    JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'
     THUMB_SIZE = (300, 300)
     SIZE_WARNING = False
     BACKEND = 'image_cropping.backends.easy_thumbs.EasyThumbnailsBackend'
